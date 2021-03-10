46a9da9cd213dcb4aca6de3c71b78476c9d22b3e
montgomeryrd <montgomery_rd@yahoo.com>
2021-03-09 20:26:11 -0800

    ES Modules in Node.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
-const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require('constants');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
 dotenv.config();
 
@@ -10,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('get request works')
-})
+});
 
 app.get('/api/products', (req, res) => {
     res.json(products)
@@ -19,8 +18,8 @@ app.get('/api/products', (req, res) => {
 app.get('/api/products/:id', (req, res) => {
     const product = products.find(product => product._id === req.params.id);
     res.json(product);
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
-})
+});
